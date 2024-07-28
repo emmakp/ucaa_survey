6b7c54e962a782af021f793a87a826af5070d0b0
Emma KP <ekipere95@gmail.com>
2024-07-28 20:24:28 +0300

    Minor bug fix

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AudienceController;
-use App\Http\Controllers\SurveyController;
-use App\Http\Controllers\QuestionaireController;
-use App\Http\Controllers\QuestionTypeController;
-use App\Http\Controllers\QuestionController;
-use App\Http\Controllers\AnswerController;
+// use App\Http\Controllers\AudienceController;
+// use App\Http\Controllers\SurveyController;
+// use App\Http\Controllers\QuestionaireController;
+// use App\Http\Controllers\QuestionTypeController;
+// use App\Http\Controllers\QuestionController;
+// use App\Http\Controllers\AnswerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -59,15 +59,21 @@ Route::group(['middleware' => 'auth'], function(){
             Route::resource('user-roles', 'UserRolesController');
             Route::resource('employees', 'EmployeesController');
             Route::resource('departments', 'DepartmentsController');
+            Route::resource('audiences', 'AudienceController');
+            Route::resource('surveys', 'SurveyController');
+            Route::resource('questionaires', 'QuestionaireController');
+            Route::resource('question-type', 'QuestionTypeController');
+            Route::resource('questions', 'QuestionController');
+            Route::resource('answers', 'AnswerController');
 
-            Route::resources([
-                'audiences' => AudienceController::class,
-                'surveys' => SurveyController::class,
-                'questionaires' => QuestionaireController::class,
-                'question-types' => QuestionTypeController::class,
-                'questions' => QuestionController::class,
-                'answers' => AnswerController::class,
-            ]);
+            // Route::resources([
+            //     'audiences' => AudienceController::class,
+            //     'surveys' => SurveyController::class,
+            //     'questionaires' => QuestionaireController::class,
+            //     'question-types' => QuestionTypeController::class,
+            //     'questions' => QuestionController::class,
+            //     'answers' => AnswerController::class,
+            // ]);
 
 
         });
