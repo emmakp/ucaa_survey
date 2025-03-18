dac136236a6ad77beec05d28d302ef01bc707556
Emmanuel Peter Kipere <ekipere95@gmail.com>
2025-03-18 14:29:54 +0300

    minor UI changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,6 +149,18 @@
                 </div>
             </div>
         </main>
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright © Your Website 2023</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>
+                        ·
+                        <a href="#">Terms & Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </div>
 @endsection
