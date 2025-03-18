8e2d1dde0fa6021d329c455db5cd55634f3c9b6a
Raymond <mwebraymond13@gmail.com>
2025-03-18 14:07:09 +0300

    adds new audiences seeded data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -152,7 +152,7 @@
         <footer class="py-4 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright © Your Website 2023</div>
+                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
                     <div>
                         <a href="#">Privacy Policy</a>
                         ·
